e12f2c96d0537cffef8154521a44f7dd291b42a6
ashis-solomon <ashissolomon24@gmail.com>
2022-10-28 21:53:23 +0530

    a check

--- app.py
+++ app.py
@@ -127,83 +127,84 @@ def chart():
 def tryout():
     return render_template('tryout.html')
 
-@app.route('/statTRY',methods=['GET'])
+@app.route('/statTRY',methods=['GET','POST'])
 def statTRY():
-    details = request.args.get("details")
-    print(details)
-    yy=json.loads(details)
-    print(yy)
-    if yy["explicit"] == "false" or yy["explicit"] == "False":
-        yy["explicit"]=0.0
-    else:
-        yy["explicit"]=1.0
-    for i in range(12):
-        yy[f'key_{i}']=0.0
-    yy[f'key_{yy["key"]}']=1.0 
-    if yy["mode"] == 1:
-        yy["mode"] = 1.0
-    else:
-        yy["mode"] = 0.0
-    yy.pop("key")
-    yy.pop("key_0")
-    print("hey")
-    print(yy)
+    if request.method == 'POST':
+        details = request.args.get("details")
+        print(details)
+        yy=json.loads(details)
+        print(yy)
+        if yy["explicit"] == "false" or yy["explicit"] == "False":
+            yy["explicit"]=0.0
+        else:
+            yy["explicit"]=1.0
+        for i in range(12):
+            yy[f'key_{i}']=0.0
+        yy[f'key_{yy["key"]}']=1.0 
+        if yy["mode"] == 1:
+            yy["mode"] = 1.0
+        else:
+            yy["mode"] = 0.0
+        yy.pop("key")
+        yy.pop("key_0")
+        print("hey")
+        print(yy)
 
-    acousticness = float(yy["acousticness"])
-    danceability = float(yy["danceability"])
-    duration_ms = float(yy["duration"])
-    energy = float(yy["energy"])
-    explicit = float(yy["explicit"])
-    instrumentalness = float(yy["instrumentalness"])
-    liveness = float(yy["liveness"])
-    loudness = float(yy["loudness"])
-    speechiness = float(yy["speechiness"])
-    tempo = float(yy["tempo"])
-    valence = float(yy["valence"])
-    #year = float(yy["year"])
-    #key_0 = yy["key_0"]
-    key_1 = yy["key_1"]
-    key_2 = yy["key_2"]
-    key_3 = yy["key_3"]
-    key_4 = yy["key_4"]
-    key_5 = yy["key_5"]
-    key_6 = yy["key_6"]
-    key_7 = yy["key_7"]
-    key_8 = yy["key_8"]
-    key_9 = yy["key_9"]
-    key_10 = yy["key_10"]
-    key_11 = yy["key_11"]
-    mode = float(yy["mode"])
-    model = pickle.load(open('model.pkl', 'rb'))
-    # model = joblib.load('statfinmodel.pkl')
-    prediction=model.predict([[acousticness,danceability,duration_ms,energy,explicit,instrumentalness,liveness,loudness,speechiness,tempo,valence,
-    key_1,key_2,key_3,key_4,key_5,key_6,key_7,key_8,key_9,key_10,key_11,mode]])
-    #prediction1=model.predict([[0.603,0.57,232013.0,0.212,0.0,0.0,0.148,-13.877,0.0437,122.513,0.182,1992.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.308159827586207,0.581103448275862,317261.637931034,0.564120689655173,0.092976407586207,0.228284482758621,-11.1943965517241,0.055008620689655,114.127551724138,0.454598275862069,28.9827586206896]])
-    #print(prediction1)
-    output=prediction[0]
-    print(output)
+        acousticness = float(yy["acousticness"])
+        danceability = float(yy["danceability"])
+        duration_ms = float(yy["duration"])
+        energy = float(yy["energy"])
+        explicit = float(yy["explicit"])
+        instrumentalness = float(yy["instrumentalness"])
+        liveness = float(yy["liveness"])
+        loudness = float(yy["loudness"])
+        speechiness = float(yy["speechiness"])
+        tempo = float(yy["tempo"])
+        valence = float(yy["valence"])
+        #year = float(yy["year"])
+        #key_0 = yy["key_0"]
+        key_1 = yy["key_1"]
+        key_2 = yy["key_2"]
+        key_3 = yy["key_3"]
+        key_4 = yy["key_4"]
+        key_5 = yy["key_5"]
+        key_6 = yy["key_6"]
+        key_7 = yy["key_7"]
+        key_8 = yy["key_8"]
+        key_9 = yy["key_9"]
+        key_10 = yy["key_10"]
+        key_11 = yy["key_11"]
+        mode = float(yy["mode"])
 
-
-
- #{'duration': 216100, 'loudness': -5.3, 'tempo': 63, 'acousticness': 0.08, 'danceability': 0.85, 'energy': 0.06, 'instrumentalness': 0.12,
- #'liveness': 0.5, 'speechiness': 0.25, 'valence': 0.54, 'mode': 1.0, 'explicit': 0.0, 'key_1': 1.0, 'key_2': 0.0, 'key_3': 0.0, 'key_4': 0.0, 'key_5': 0.0,
- #'key_6': 0.0, 'key_7': 0.0, 'key_8': 0.0, 'key_9': 0.0, 'key_10': 0.0, 'key_11': 0.0}
+        model = joblib.load('statfinmodel.pkl')
+        prediction=model.predict([[acousticness,danceability,duration_ms,energy,explicit,instrumentalness,liveness,loudness,speechiness,tempo,valence,
+        key_1,key_2,key_3,key_4,key_5,key_6,key_7,key_8,key_9,key_10,key_11,mode]])
+        #prediction1=model.predict([[0.603,0.57,232013.0,0.212,0.0,0.0,0.148,-13.877,0.0437,122.513,0.182,1992.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.308159827586207,0.581103448275862,317261.637931034,0.564120689655173,0.092976407586207,0.228284482758621,-11.1943965517241,0.055008620689655,114.127551724138,0.454598275862069,28.9827586206896]])
+        #print(prediction1)
+        output=prediction[0]
+        print(output)
 
 
 
-  # Index(['acousticness', 'danceability', 'duration_ms', 'energy', 'explicit',
-   #    'instrumentalness', 'liveness', 'loudness', 'popularity', 'speechiness',
-    #   'tempo', 'valence', 'year', 'key_1', 'key_2', 'key_3', 'key_4', 'key_5',
-     #  'key_6', 'key_7', 'key_8', 'key_9', 'key_10', 'key_11', 'mode'],
-      #dtype='object')
+    #{'duration': 216100, 'loudness': -5.3, 'tempo': 63, 'acousticness': 0.08, 'danceability': 0.85, 'energy': 0.06, 'instrumentalness': 0.12,
+    #'liveness': 0.5, 'speechiness': 0.25, 'valence': 0.54, 'mode': 1.0, 'explicit': 0.0, 'key_1': 1.0, 'key_2': 0.0, 'key_3': 0.0, 'key_4': 0.0, 'key_5': 0.0,
+    #'key_6': 0.0, 'key_7': 0.0, 'key_8': 0.0, 'key_9': 0.0, 'key_10': 0.0, 'key_11': 0.0}
 
 
 
+    # Index(['acousticness', 'danceability', 'duration_ms', 'energy', 'explicit',
+    #    'instrumentalness', 'liveness', 'loudness', 'popularity', 'speechiness',
+        #   'tempo', 'valence', 'year', 'key_1', 'key_2', 'key_3', 'key_4', 'key_5',
+        #  'key_6', 'key_7', 'key_8', 'key_9', 'key_10', 'key_11', 'mode'],
+        #dtype='object')
 
 
 
 
-    #print(x)
+
+
+
+        #print(x)
     return render_template('statTRY.html',output=output)
 
 @app.route('/about')
