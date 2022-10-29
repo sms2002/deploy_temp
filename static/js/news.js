075f4849b0ea99cf420f7c2d9287b5912a6ce053
ashis-solomon <ashissolomon24@gmail.com>
2022-10-29 21:26:02 +0530

    a check

--- static/js/news.js
+++ static/js/news.js
@@ -25,44 +25,34 @@ function setINITIALnews(){
     console.log(urlarr)
     console.log(imagesarr)
     
-    let api_key = "5ded574a864b45148274ffb0adc49f2c";
-    console.log('APII')
-    let curr_url = `https://newsapi.org/v2/everything?q=music&domains=billboard.com&language=en&apiKey=${api_key}`
-
-    let stringfornews_0 = `storage_0_`;
-    let stringfornews_1 = `storage_1_`;
-    let stringfornews_2 = `storage_2_`;
-
-    fetch(curr_url).then((res)=>{
-        return res.json()
-    }).then((data)=>{
-       return data["articles"]
-    }).then((articles)=>{
-       for(let i=0;i<9;i++){
-        //    let temp = `${stringfornews_0}_${i}`
-        //    console.log(temp)
-           localStorage.setItem(`${stringfornews_0}${i}`,articles[i]["title"])
-           console.log(articles[i]["title"])
+    for(let i=0;i<9;i++){
+        // //    let temp = `${stringfornews_0}_${i}`
+        // //    console.log(temp)
+        //    localStorage.setItem(`${stringfornews_0}${i}`,articles[i]["title"])
+        //    console.log(articles[i]["title"])
     
-           localStorage.setItem(`${stringfornews_1}${i}`,articles[i]["url"])
-           console.log(articles[i]["url"])
+        //    localStorage.setItem(`${stringfornews_1}${i}`,articles[i]["url"])
+        //    console.log(articles[i]["url"])
     
-           localStorage.setItem(`${stringfornews_2}${i}`,articles[i]["urlToImage"])
-           console.log(articles[i]["urlToImage"])
+        //    localStorage.setItem(`${stringfornews_2}${i}`,articles[i]["urlToImage"])
+        //    console.log(articles[i]["urlToImage"])
            
            let card_id=`cards${i}`;
            var y = document.getElementById(`${card_id}`);
-           y.querySelector("img").src=articles[i]["urlToImage"];
-           y.querySelector("h4").innerHTML=articles[i]["title"];
-           y.querySelector("img").src=articles[i]["urlToImage"];
+        //    y.querySelector("img").src=articles[i]["urlToImage"];
+        //    y.querySelector("h4").innerHTML=articles[i]["title"];
+        //    y.querySelector("img").src=articles[i]["urlToImage"];
            
+           y.querySelector("img").src=urlarr[i];
+           y.querySelector("h4").innerHTML=titlearr[i];
+           y.querySelector("img").src=imagesarr[i];
     
            let linkcard_id=`linkcards${i}`;
            var z = document.getElementById(`${linkcard_id}`);
-           z.querySelector("a").href=articles[i]["url"];
+           z.querySelector("a").href=urlarr[i];
     
-       }
-    });
+       }  
+   
 }
 
 function callNEWS(){
